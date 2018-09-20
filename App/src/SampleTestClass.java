2b1611f6a2ec429e0fa7bf0f79197404cd0b412b
ncaron1 <ncaron1@id415m50.cs.unb.ca>
2018-09-20 15:12:37 -0300

    Project CS2043 Soft. Eng. 	\------>

---
+++ App/src/SampleTestClass.java
@@ -0,0 +1,5 @@
+public class SampleTestClass {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ App/src/module-info.java
@@ -0,0 +1,5 @@
+public class sampleTestClass{
+	public static void main(String[] args) {
+		
+	}
+}
